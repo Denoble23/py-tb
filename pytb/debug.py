1791a0b163cf199518667c6bbfb26877fb0e3e62
Matthew Miglio <matmigg0804@gmail.com>
2022-09-28 19:48:28 -0400

    idk

--- pytb/debug.py
+++ pytb/debug.py
@@ -8,12 +8,16 @@ import pyautogui
 import pyperclip
 from matplotlib import pyplot as plt
 
-from pytb.client import (check_for_blacklist_in_text, get_name_of_current_profile,
-                         get_this_profiles_bio_text, get_to_random_account_from_followers_list, get_to_random_account_from_followers_list_with_blacklist, screenshot)
+from pytb.client import (
+    check_for_blacklist_in_text, get_name_of_current_profile,
+    get_this_profiles_bio_text, get_to_random_account_from_followers_list,
+    get_to_random_account_from_followers_list_with_blacklist,
+    orientate_edge_window, screenshot)
 from pytb.configuration import load_user_settings
 from pytb.database import Database
 from pytb.image_rec import (check_for_location, coords_is_equal,
-                            find_references, get_first_location, pixel_is_equal)
+                            find_references, get_first_location,
+                            pixel_is_equal)
 from pytb.logger import Logger
 
 logger = Logger()
@@ -29,6 +33,3 @@ launcher_path = user_settings["launcher_path"]
 
 
 
-
-name="test"
-print(users_ive_followed_from_database.check_if_user_in_users_followed_database(name))
