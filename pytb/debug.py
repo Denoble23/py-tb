18e5a88060c1ae8cd265eda1a3962749303998d7
Matthew Stream-PC <matmigg080@gmail.com>
2022-09-28 22:35:51 -0400

    idk

--- pytb/debug.py
+++ pytb/debug.py
@@ -23,12 +23,15 @@ users_ive_followed_from_database = Database("users_ive_followed_from")
 user_settings = load_user_settings()
 launcher_path = user_settings["launcher_path"]
 
-orientate_edge_window(logger)
+# orientate_edge_window(logger)
 
-show_image(screenshot(region=[0,0,1400,2000]))
+# show_image(screenshot(region=[0,0,1400,2000]))
 
       
 # print(find_follow_buttons())
-    
+
+
+bio_text=get_this_profiles_bio_text()
+print(bio_text)
 
 
